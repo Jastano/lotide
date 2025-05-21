fcc33792a245c39c85135941fe72947fe605ab7f
JasTano <jasmine.broodhagen@outlook.com>
2025-05-21 15:26:03 -0400

    tail Implemented

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Tests
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);           
+assertEqual(result[0], "Lighthouse");    
+assertEqual(result[1], "Labs");          
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // Not capturing the return value here
+assertEqual(words.length, 3);       
