d49deb4451e7805d851c00c1bd83c5f35b56bf3c
JasTano <jasmine.broodhagen@outlook.com>
2025-05-23 18:53:04 -0400

    without function added

---
+++ without.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
+    }
+  }
+
+  return result;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
