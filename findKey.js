b62ff1f72a21b5b08bd50a9771defb06b86fb7e8
JasTano <jasmine.broodhagen@outlook.com>
2025-05-24 02:11:08 -0400

    findKey function added

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const testData = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+assertEqual(findKey(testData, x => x.stars === 2), "noma");
+assertEqual(findKey(testData, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(testData, x => x.stars === 5), undefined);
